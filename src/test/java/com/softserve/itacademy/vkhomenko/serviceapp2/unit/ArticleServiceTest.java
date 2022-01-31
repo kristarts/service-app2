f2171a6495b3b029c4155871ba3e23afc1deda6c
kristarts <kristarts@gmail.com>
2022-01-31 20:20:26 +0200

    Test implementation

---
+++ src/test/java/com/softserve/itacademy/vkhomenko/serviceapp2/unit/ArticleServiceTest.java
@@ -0,0 +1,65 @@
+package com.softserve.itacademy.vkhomenko.serviceapp2.unit;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * ArticleService Tester.
+ *
+ * @author <Authors name>
+ * @since <pre>Jan 31, 2022</pre>
+ * @version 1.0
+ */
+public class ArticleServiceTest {
+
+    @Before
+    public void before() throws Exception {
+    }
+
+
+        @After
+        public void after() throws Exception {
+        }
+
+        /**
+         *
+         * Method: getAllArticlesFromUser(UUID userId, int page)
+         *
+         */
+        @Test
+        public void testGetAllArticlesFromUser() throws Exception {
+//TODO: Test goes here...
+        }
+
+        /**
+         *
+         * Method: createArticle(UUID userId, ArticleCreateRequestDto articleCreateRequestDto)
+         *
+         */
+        @Test
+        public void testCreateArticle() throws Exception {
+//TODO: Test goes here...
+        }
+
+        /**
+         *
+         * Method: getArticleById(UUID articleId)
+         *
+         */
+        @Test
+        public void testGetArticleById() throws Exception {
+//TODO: Test goes here...
+        }
+
+        /**
+         *
+         * Method: deleteArticleById(UUID userId, UUID articleId)
+         *
+         */
+        @Test
+        public void testDeleteArticleById() throws Exception {
+//TODO: Test goes here...
+        }
+
+}
